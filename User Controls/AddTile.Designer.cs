4c55606da6a59528b3a0998def891156851b426f
Yuval <yuvalgal51@gmail.com>
2018-02-03 16:12:21 +0200

    Added refresh button

--- User Controls/AddTile.Designer.cs
+++ User Controls/AddTile.Designer.cs
@@ -28,8 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.btnRefresh = new System.Windows.Forms.Button();
             this.btnAdd = new System.Windows.Forms.Button();
+            this.tableLayoutPanel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tableLayoutPanel
+            // 
+            this.tableLayoutPanel.ColumnCount = 2;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 86.22449F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 13.77551F));
+            this.tableLayoutPanel.Controls.Add(this.btnAdd, 0, 0);
+            this.tableLayoutPanel.Controls.Add(this.btnRefresh, 1, 0);
+            this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.RowCount = 1;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel.Size = new System.Drawing.Size(784, 53);
+            this.tableLayoutPanel.TabIndex = 0;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(140)))), ((int)(((byte)(177)))));
+            this.btnRefresh.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRefresh.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnRefresh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRefresh.Font = new System.Drawing.Font("Calibri", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRefresh.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnRefresh.Location = new System.Drawing.Point(678, 3);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(103, 47);
+            this.btnRefresh.TabIndex = 4;
+            this.btnRefresh.Text = "🔄";
+            this.btnRefresh.UseVisualStyleBackColor = false;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
             // 
             // btnAdd
             // 
@@ -39,10 +73,10 @@
             this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnAdd.Font = new System.Drawing.Font("Calibri", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnAdd.ForeColor = System.Drawing.SystemColors.Control;
-            this.btnAdd.Location = new System.Drawing.Point(0, 0);
+            this.btnAdd.Location = new System.Drawing.Point(3, 3);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(784, 53);
-            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Size = new System.Drawing.Size(669, 47);
+            this.btnAdd.TabIndex = 6;
             this.btnAdd.Text = "➕";
             this.btnAdd.UseVisualStyleBackColor = false;
             this.btnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
@@ -51,15 +85,18 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.tableLayoutPanel);
             this.Name = "AddTile";
             this.Size = new System.Drawing.Size(784, 53);
+            this.tableLayoutPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
         private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRefresh;
     }
 }
--- User Controls/AddTile.cs
+++ User Controls/AddTile.cs
@@ -21,7 +21,36 @@ namespace AlbertEinsteinCommunity
 
         private void BtnAdd_Click(object sender, EventArgs e)
         {
-            welcomeForm.Wysiwyg();
+            RTFEditorForm rtfEditorForm = new RTFEditorForm(welcomeForm.replyCreation);
+            rtfEditorForm.ShowDialog();
+            if (rtfEditorForm.isEdited)
+                if (welcomeForm.replyCreation)
+                {
+                    Reply newReply = new Reply();
+                    newReply.ReplyMaker = welcomeForm.user;
+                    newReply.ReplyContent = rtfEditorForm.rtf;
+                    newReply.ThreadId = welcomeForm.currentThreadId;
+                    new Controller().AddReply(newReply);
+                    welcomeForm.SetReplyList(welcomeForm.currentThreadId);
+                }
+                else
+                {
+                    Thread newThread = new Thread();
+                    newThread.ThreadMaker = welcomeForm.user;
+                    newThread.ThreadName = rtfEditorForm.threadName;
+                    newThread.ThreadContent = rtfEditorForm.rtf;
+                    newThread.ForumName = welcomeForm.currentForumName;
+                    new Controller().AddThread(newThread);
+                    welcomeForm.SetThreadList(welcomeForm.currentForumName);
+                }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            if (welcomeForm.replyCreation)
+                welcomeForm.SetReplyList(welcomeForm.currentThreadId);
+            else
+                welcomeForm.SetThreadList(welcomeForm.currentForumName);
         }
     }
 }
